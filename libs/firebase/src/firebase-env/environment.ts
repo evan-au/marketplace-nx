41833c1a4ad91f9bca16639ac7f090bbbbf994f6
Evan Pierre Louis <evan.dev@yandex.com>
2022-05-05 15:10:48 +0200

    lib:firebase: Add firebase to project

---
+++ libs/firebase/src/firebase-env/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'marketplace-b10bc',
+    appId: '1:765758369612:web:92d68a9a825b97de74a9e9',
+    storageBucket: 'marketplace-b10bc.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBO90qZpGwuPA2cql-u7zB8awT58ajhn-c',
+    authDomain: 'marketplace-b10bc.firebaseapp.com',
+    messagingSenderId: '765758369612',
+    measurementId: 'G-K7D9F2KT1M',
+  },
+  production: false,
+};
--- libs/firebase/src/lib/firebase.module.ts
+++ libs/firebase/src/lib/firebase.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Modules
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../firebase-env/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+  ],
 })
 export class FirebaseModule {}

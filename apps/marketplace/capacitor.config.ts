f27cf46fe0f57c5ca5768de478a50bb5d3a858ac
Evan Pierre Louis <evan.dev@yandex.com>
2022-05-05 13:50:44 +0200

    chore: Add Ionic app to project

---
+++ apps/marketplace/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'marketplace',
+  webDir: '../../dist/apps/marketplace',
+  bundledWebRuntime: false,
+};
+
+export default config;

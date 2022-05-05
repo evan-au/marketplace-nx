45a291e1828b0d8b7749c0b5f667102ee5c5a337
Evan Pierre Louis <evan.dev@yandex.com>
2022-05-05 14:32:42 +0200

    app: Add home and add-item routes

--- apps/marketplace/src/app/tabs/tabs-routing.module.ts
+++ apps/marketplace/src/app/tabs/tabs-routing.module.ts
@@ -7,31 +7,26 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      // {
-      //   path: 'tab1',
-      //   loadChildren: () =>
-      //     import('../tab1/tab1.module').then((m) => m.Tab1PageModule),
-      // },
-      // {
-      //   path: 'tab2',
-      //   loadChildren: () =>
-      //     import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
-      // },
-      // {
-      //   path: 'tab3',
-      //   loadChildren: () =>
-      //     import('../tab3/tab3.module').then((m) => m.Tab3PageModule),
-      // },
-      // {
-      //   path: '',
-      //   redirectTo: '/tabs/tab1',
-      //   pathMatch: 'full',
-      // },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('@marketplace-nx/home').then((m) => m.HomeModule),
+      },
+      {
+        path: 'add-item',
+        loadChildren: () =>
+          import('@marketplace-nx/add-item').then((m) => m.AddItemModule),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full',
+      },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs',
+    redirectTo: '/tabs/home',
     pathMatch: 'full',
   },
 ];

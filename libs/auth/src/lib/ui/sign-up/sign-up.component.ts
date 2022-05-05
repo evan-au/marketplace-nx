ad77f7fa1d08faa00954e1573a895ea772ad017f
Evan Pierre Louis <evan.dev@yandex.com>
2022-05-05 14:50:06 +0200

    lib:auth:ui: Implement sign-in and sign-up components

---
+++ libs/auth/src/lib/ui/sign-up/sign-up.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auth-sign-up',
+  templateUrl: './sign-up.component.html',
+})
+export class SignUpComponent implements OnInit {
+  // constructor() { }
+
+  ngOnInit() {
+    return;
+  }
+}

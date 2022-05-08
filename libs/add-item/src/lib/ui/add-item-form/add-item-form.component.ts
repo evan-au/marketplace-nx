e78de122d273d9af03f26c27a820be2774f6321c
Evan Pierre Louis <evan.dev@yandex.com>
2022-05-08 17:04:35 +0200

    lib:add-item:ui: Implement swiper form

--- libs/add-item/src/lib/ui/add-item-form/add-item-form.component.ts
+++ libs/add-item/src/lib/ui/add-item-form/add-item-form.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+// Swiper
+import Swiper, { Navigation, SwiperOptions } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'add-item-form',
@@ -6,9 +15,19 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddItemFormComponent implements OnInit {
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+
+  public config: SwiperOptions = {};
   // constructor() {}
 
   ngOnInit(): void {
-    return;
+    Swiper.use([Navigation]);
+  }
+
+  slideNext() {
+    this.swiper?.swiperRef.slideNext(100);
+  }
+  slidePrev() {
+    this.swiper?.swiperRef.slidePrev(100);
   }
 }
